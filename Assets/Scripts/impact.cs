e2081aa35bdb7a7df5805897096e169c7bf352b9
DESKTOP-7A8C176\Matthias <zhyyubbh@clrmail.com>
2017-08-26 21:55:25 +0200

    Projectile Impact and Self-Destruct

---
+++ Assets/Scripts/impact.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class impact : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Tank")
+        {
+            // obst.isShaking = true;
+            TankMovementScript tankScript = other.gameObject.GetComponent<TankMovementScript>();
+            tankScript.MakeVisible();
+
+        }
+
+
+
+        Destroy(transform.root.gameObject);
+
+    }
+
+
+
+}
